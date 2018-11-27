bfefb8be46e02c7aeff7a1048fae62191e491b21
Ivens Diego Müller <ivensdiegomuller@gmail.com>
2018-11-27 19:58:41 -0200

    Server Main

---
+++ Projetos/DrillerZapServer/src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+import Server.Server;
+
+/**
+ *
+ * @author Ivens
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.execute();
+    }
+    
+}
