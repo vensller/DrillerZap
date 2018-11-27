1e73e2e0a03892cc75780d684365914446916401
paulohenriquerodrigues <rodriguespaulo1997@gmail.com>
2018-11-27 19:59:01 -0200

    Main

---
+++ Projetos/DrillerZap/src/Main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import View.Login;
+
+/**
+ *
+ * @author Paulo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
