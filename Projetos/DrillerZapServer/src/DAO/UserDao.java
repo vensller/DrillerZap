e9b56d1d9e12aed9507c1d96e3d4f8769f4321d9
Ivens Diego Müller <ivensdiegomuller@gmail.com>
2018-11-27 21:57:56 -0200

    SQL

--- Projetos/DrillerZapServer/src/DAO/UserDao.java
+++ Projetos/DrillerZapServer/src/DAO/UserDao.java
@@ -7,10 +7,10 @@ package DAO;
 public class UserDao {
     
     private String sqlInsert = "INSERT INTO USERS (NAME, EMAIL, PASSWORD, TELEPHONE) VALUES(?,?,?,?);";
-    private String sqlUpdate = "";
-    private String sqlDelete = "";
-    private String sqlFind = "";
-    private String sqlFindEmail = "";
+    private String sqlUpdate = "UPDATE USERS A SET A.NAME = ?, A.EMAIL = ?, A.PASSWORD = ?, A.TELEPHONE = ? WHERE A.ID = ?;";
+    private String sqlDelete = "DELETE USERS WHERE ID = ?;";
+    private String sqlFind = "SELECT * FROM USERS WHERE ID = ?;";
+    private String sqlFindEmail = "SELECT * FROM USERS WHERE EMAIL = ?;";
     
     
 }
