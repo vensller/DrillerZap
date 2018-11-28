0be366ca0efe4c5faaae172658bad827d60eea82
paulohenriquerodrigues <rodriguespaulo1997@gmail.com>
2018-11-28 20:08:43 -0200

    Jframe Abrir no Centro da Tela

--- Projetos/DrillerZap/src/View/Login.java
+++ Projetos/DrillerZap/src/View/Login.java
@@ -10,6 +10,8 @@ public class Login extends javax.swing.JFrame {
 
     public Login() {
         initComponents();
+        this.setLocationRelativeTo(null);
+
     }
 
     @SuppressWarnings("unchecked")
--- Projetos/DrillerZap/src/View/Register.java
+++ Projetos/DrillerZap/src/View/Register.java
@@ -16,6 +16,8 @@ public class Register extends javax.swing.JFrame implements ApprovedObserver{
 
     public Register() {
         initComponents();
+        this.setLocationRelativeTo(null);
+
     }
 
     public boolean validaCampos() {
