337ab5eea478c23ff1383d2e2f9d1498ba720283
paulohenriquerodrigues <rodriguespaulo1997@gmail.com>
2018-11-28 20:35:21 -0200

    Update Projetos/DrillerZap/src/View/Login.java

--- Projetos/DrillerZap/src/View/Login.java
+++ Projetos/DrillerZap/src/View/Login.java
@@ -16,7 +16,6 @@ public class Login extends javax.swing.JFrame {
     public Login() {
         initComponents();
         this.setLocationRelativeTo(null);
-
     }
 
     @SuppressWarnings("unchecked")
